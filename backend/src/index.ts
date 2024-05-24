f6b12fa3c9ddb9dc3551a156b72b631a99261b26
Krishna <krishanmohank974@gmail.com>
2024-05-24 21:16:32 +0530

    kuch to

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,49 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+// const prisma = new PrismaClient({            this should be initilize in each and evry route for
+//     datasourceUrl: env.DATABASE_URL,
+// }).$extends(withAccelerate()) 
 
-app.get('/', (c) => {
+// <{
+//   Bindings: {                      defining the type  
+//   DATABASE_URL: string
+//   }
+// }>
+
+const app = new Hono<{
+  Bindings: {
+  DATABASE_URL: string
+  }
+}>()
+
+app.get('/',  (c) => {
+
   return c.text('Hello Hono!')
 })
 
-app.post('/api/v1/user/signup', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/user/signup',async  (c) => {
+  const body = await c.req.json();
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+
+//zod, hash password
+try{
+  await prisma.user.create({
+    data: {
+      name: body.name,
+      username: body.username,
+      password: body.password,
+    },
+})
+
+return c.text('Signed Up!');
+} catch (e) {
+   c.status(411); c.text("Invalid input");
+}
 })   
 
 app.post('/api/v1/user/signin', (c) => {
