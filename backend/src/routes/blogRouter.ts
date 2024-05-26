233579121946b57c5ba5d3627fd7d64854c8964b
Krishna <krishanmohank974@gmail.com>
2024-05-27 02:43:52 +0530

    added zod in user and blog routes and updated readme

--- backend/src/routes/blogRouter.ts
+++ backend/src/routes/blogRouter.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { verify } from "hono/jwt";
 import { Hono } from "hono";
+import { CreateBlogInput, createblogInput, updateBlogInput } from "@krishna7060/common";}
 
 export const blogRouter = new Hono<
 {
@@ -31,6 +32,13 @@ blogRouter.use("/*", async (c, next) => {
 blogRouter.post('/', async (c) => {
 
     const body = await c.req.json();
+    const {success} = createblogInput.safeParse(body);
+    if (!success){
+      c.status(411);
+      return c.json({
+        message: "Inputs are not correct"
+      })
+    }
     const userId = c.get("userId");
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
@@ -48,6 +56,13 @@ blogRouter.post('/', async (c) => {
   
   blogRouter.put('/', async (c) => {
     const body = await c.req.json();
+    const {success} = updateBlogInput.safeParse(body);
+    if (!success){
+      c.status(411);
+      return c.json({
+        message: "Inputs are not correct"
+      })
+    }
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import {decode, sign, verify} from 'hono/jwt'
-import { signupInput } from "@krishna7060/common";
+import { signupInput, LoginInput } from "@krishna7060/common";
 
 
 const userRouter = new Hono<{
@@ -45,7 +45,13 @@ userRouter.post('/signup',async  (c) => {
   
   userRouter.post('/signin', async (c) => {
     const body = await c.req.json();
-  
+  const {success} = signupInput.safeParse(body);
+  if(!success) {
+    c.status(411)
+    return c.json({
+      message: "inputs not valid"
+    })
+  }
     const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
