7d0ff53d39057e6f8d5ff888272e4e817481e1a8
Krishna <krishanmohank974@gmail.com>
2024-05-28 03:29:41 +0530

    added frontend routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
 
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Signin } from './pages/Signin'
+import { Signup } from './pages/Signup'
+import { Blog } from './pages/Blog'
 
 function App() {
 
 
   return (
     <>
-  hello
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/blog/:id" element={<Blog />} />
+    </Routes>
+  </BrowserRouter>
     </>
   )
 }
