0a6e9081e12699872ee7f24f2ec7d1db659686d5
Krishna <krishanmohank974@gmail.com>
2024-05-28 19:13:17 +0530

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import userRouter from './routes/userRouter';
 import { blogRouter } from './routes/blogRouter';
+import { cors } from 'hono/cors';
 
 
 // const prisma = new PrismaClient({            this should be initilize in each and evry route for
@@ -14,7 +15,7 @@ import { blogRouter } from './routes/blogRouter';
 // }>
 
 const app = new Hono()
-
+app.use('/*', cors())
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
 
