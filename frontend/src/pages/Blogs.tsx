d3b19b426b5bc4e48dd1cf136d2d0fd5f6804c5e
Krishna <krishanmohank974@gmail.com>
2024-08-19 03:14:14 +0530

    fixed error and created blogCard

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,7 +1,18 @@
 import { BlogCard } from "../components/BlogCard"
-
+import {Appbar} from "../components/AppBar"
 export const  Blogs = () => {
     return <div>
+        <Appbar  />
+         <div className="flex justify-center">
+    <div className=" max-w-xl">
         <BlogCard id={1} publishedDate={"10/10/2003"} authorName={"krishna"} title={"aslkfjasklfjliasl"} content={"jfajflsalkfasdfkjasdasdflasflkasfkljsadkjlfalskflkasjdflakjsfdnjasdlkflkc akshv glshklvhcs akfvh klahkfjas"} />
+        <BlogCard id={2} publishedDate={"10/10/2003"} authorName={"krishna"} title={"aslkfjasklfjliasl"} content={"jfajflsalkfasdfkjasdasdflasflkasfkljsadkjlfalskflkasjdflakjsfdnjasdlkflkc akshv glshklvhcs akfvh klahkfjas"} />
+        <BlogCard id={3} publishedDate={"10/10/2003"} authorName={"krishna"} title={"aslkfjasklfjliasl"} content={"jfajflsalkfasdfkjasdasdflasflkasfkljsadkjlfalskflkasjdflakjsfdnjasdlkflkc akshv glshklvhcs akfvh klahkfjas"} />
+        <BlogCard id={4} publishedDate={"10/10/2003"} authorName={"krishna"} title={"aslkfjasklfjliasl"} content={"jfajflsalkfasdfkjasdasdflasflkasfkljsadkjlfalskflkasjdflakjsfdnjasdlkflkc akshv glshklvhcs akfvh klahkfjas"} />
+        <BlogCard id={5} publishedDate={"10/10/2003"} authorName={"krishna"} title={"aslkfjasklfjliasl"} content={"jfajflsalkfasdfkjasdasdflasflkasfkljsadkjlfalskflkasjdflakjsfdnjasdlkflkc akshv glshklvhcs akfvh klahkfjas"} />
+        <BlogCard id={6} publishedDate={"10/10/2003"} authorName={"krishna"} title={"aslkfjasklfjliasl"} content={"jfajflsalkfasdfkjasdasdflasflkasfkljsadkjlfalskflkasjdflakjsfdnjasdlkflkc akshv glshklvhcs akfvh klahkfjas"} />
+        <BlogCard id={7} publishedDate={"10/10/2003"} authorName={"krishna"} title={"aslkfjasklfjliasl"} content={"jfajflsalkfasdfkjasdasdflasflkasfkljsadkjlfalskflkasjdflakjsfdnjasdlkflkc akshv glshklvhcs akfvh klahkfjas"} />
+    </div>
+    </div>
     </div>
 }
