0672f99aec27d225bb7bcd2de5f9e2f4a5169904
Krishna <krishanmohank974@gmail.com>
2024-08-19 04:22:13 +0530

    added hooks, and fetching all blogs on blog card

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,17 +1,21 @@
 import { BlogCard } from "../components/BlogCard"
 import {Appbar} from "../components/AppBar"
+import {useBlogs} from "../hooks"
 export const  Blogs = () => {
+    const {loading ,blogs } = useBlogs();
+
+    if (loading){
+        return <div>
+            Loading...
+        </div>
+    }
     return <div>
         <Appbar  />
          <div className="flex justify-center">
     <div className=" max-w-xl">
+        {blogs.map(blog => <BlogCard id={blog.id} publishedDate={blog.publishedDate} authorName={blog.author.name || "anonymous"} title={blog.title} content={blog.content} />)}
         <BlogCard id={1} publishedDate={"10/10/2003"} authorName={"krishna"} title={"aslkfjasklfjliasl"} content={"jfajflsalkfasdfkjasdasdflasflkasfkljsadkjlfalskflkasjdflakjsfdnjasdlkflkc akshv glshklvhcs akfvh klahkfjas"} />
-        <BlogCard id={2} publishedDate={"10/10/2003"} authorName={"krishna"} title={"aslkfjasklfjliasl"} content={"jfajflsalkfasdfkjasdasdflasflkasfkljsadkjlfalskflkasjdflakjsfdnjasdlkflkc akshv glshklvhcs akfvh klahkfjas"} />
-        <BlogCard id={3} publishedDate={"10/10/2003"} authorName={"krishna"} title={"aslkfjasklfjliasl"} content={"jfajflsalkfasdfkjasdasdflasflkasfkljsadkjlfalskflkasjdflakjsfdnjasdlkflkc akshv glshklvhcs akfvh klahkfjas"} />
-        <BlogCard id={4} publishedDate={"10/10/2003"} authorName={"krishna"} title={"aslkfjasklfjliasl"} content={"jfajflsalkfasdfkjasdasdflasflkasfkljsadkjlfalskflkasjdflakjsfdnjasdlkflkc akshv glshklvhcs akfvh klahkfjas"} />
-        <BlogCard id={5} publishedDate={"10/10/2003"} authorName={"krishna"} title={"aslkfjasklfjliasl"} content={"jfajflsalkfasdfkjasdasdflasflkasfkljsadkjlfalskflkasjdflakjsfdnjasdlkflkc akshv glshklvhcs akfvh klahkfjas"} />
-        <BlogCard id={6} publishedDate={"10/10/2003"} authorName={"krishna"} title={"aslkfjasklfjliasl"} content={"jfajflsalkfasdfkjasdasdflasflkasfkljsadkjlfalskflkasjdflakjsfdnjasdlkflkc akshv glshklvhcs akfvh klahkfjas"} />
-        <BlogCard id={7} publishedDate={"10/10/2003"} authorName={"krishna"} title={"aslkfjasklfjliasl"} content={"jfajflsalkfasdfkjasdasdflasflkasfkljsadkjlfalskflkasjdflakjsfdnjasdlkflkc akshv glshklvhcs akfvh klahkfjas"} />
+        
     </div>
     </div>
     </div>
